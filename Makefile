8190055e582fc7a09a7f4a4149961495d3324ee3
infl4me <hellpl4y@gmail.com>
2019-03-27 01:45:43 +0500

    init

---
+++ Makefile
@@ -0,0 +1,20 @@
+start:
+	npm start
+
+create:
+	npx babel-node createComponent.js
+
+install:
+	npm install
+
+build:
+	npm run build
+
+test:
+	npm test
+
+lint:
+	npx eslint .
+
+deploy:
+	npm run deploy
---
+++ createComponent.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+
+const camelize = str => str.split('-').map(s => `${s[0].toUpperCase()}${s.slice(1).toLowerCase()}`).join('');
+
+const fn = (name) => {
+  const path = `./src/components/${name}`;
+  fs.mkdirSync(path);
+  const indexData = `import ${camelize(name)} from './${name}';
+
+export default ${camelize(name)};
+`;
+  const jsData = `import React from 'react';
+import './${name}.css';
+
+export default class ${camelize(name)} extends React.Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
+`;
+  fs.writeFileSync(`${path}/${name}.js`, jsData);
+  fs.writeFileSync(`${path}/${name}.css`, '');
+  fs.writeFileSync(`${path}/index.js`, indexData);
+};
+
+fn('app');
