4b0770580f9f9958d64614a9d5275f183ec040a7
infl4me <hellpl4y@gmail.com>
2019-03-28 09:55:07 +0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/star-db/precache-manifest.f15a1d3be57b818b967c211c44bab8a7.js"
+  "/star-db/precache-manifest.78326d83d57f50ffb315e3e4146c0e28.js"
 );
 
 workbox.clientsClaim();
